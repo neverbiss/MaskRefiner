55d4452fe73ab4886469e8640c940eb842e86f73
neverbiss <100753803+neverbiss@users.noreply.github.com>
2026-06-12 14:23:21 +0800

    feat: create maskrefiner package

---
+++ maskrefiner/sam_refiner.py
@@ -0,0 +1,207 @@
+"""
+SAM-based iterative mask refinement.
+
+Supports SAM (ViT-H) and HQ-SAM backends. The refiner generates
+noise-tolerant prompts (points, boxes, Gaussian mask priors) from
+coarse masks and iteratively refines them through SAM's decoder.
+"""
+
+import cv2
+import numpy as np
+import torch
+from segment_anything.utils.transforms import ResizeLongestSide
+
+from .utils import extract_bboxes_expand, extract_mask, extract_points, prepare_image
+
+
+def sam_input_prepare(
+    image,
+    pred_masks,
+    image_embeddings=None,
+    resize_transform=None,
+    use_point=True,
+    use_box=True,
+    use_mask=True,
+    add_neg=True,
+    margin=0.0,
+    gamma=1.0,
+    strength=15,
+):
+    """Prepare SAM decoder inputs from predicted masks."""
+    ori_size = pred_masks.shape[-2:]
+    input_dict = {
+        "image": image,
+        "original_size": ori_size,
+    }
+
+    target_size = image.shape[1:]
+    expand_list = torch.zeros((len(pred_masks))).to(image.device)
+    if use_box:
+        bboxes, box_masks, areas, expand_list = extract_bboxes_expand(
+            image_embeddings, pred_masks, margin=margin
+        )
+        input_dict["boxes"] = resize_transform.apply_boxes_torch(bboxes, ori_size)
+
+    point_coords, point_labels, gaus_dt = extract_points(
+        pred_masks, add_neg=add_neg, use_mask=use_mask, gamma=gamma
+    )
+    if use_point:
+        input_dict["point_coords"] = resize_transform.apply_coords_torch(
+            point_coords, ori_size
+        )
+        input_dict["point_labels"] = point_labels
+
+    if use_mask:
+        input_dict["mask_inputs"] = extract_mask(
+            pred_masks,
+            gaus_dt,
+            target_size,
+            is01=True,
+            strength=strength,
+            device=image.device,
+            expand_list=expand_list,
+        )
+
+    return input_dict, point_coords
+
+
+def sam_refiner(
+    image_path,
+    coarse_masks,
+    sam,
+    resize_transform=None,
+    use_point=True,
+    use_box=True,
+    use_mask=True,
+    add_neg=True,
+    iters=5,
+    margin=0.0,
+    gamma=4.0,
+    strength=30,
+    use_samhq=False,
+    ddp=False,
+    is_train=False,
+):
+    """Iteratively refine coarse masks using SAM."""
+    if isinstance(coarse_masks, list):
+        coarse_masks = np.stack(coarse_masks, axis=0)
+
+    if len(coarse_masks.shape) == 2:
+        coarse_masks = coarse_masks[None:,]
+    coarse_masks = torch.tensor(coarse_masks, dtype=torch.uint8).to(sam.device)
+    assert (
+        len(coarse_masks.shape) == 3
+    ), f"coarse mask dim must be (n, h, w), but got {coarse_masks.shape}"
+
+    if resize_transform is None:
+        resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
+
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = [prepare_image(image, resize_transform, sam.device)]
+
+    with torch.no_grad():
+        if ddp:
+            input_images = torch.stack([sam.module.preprocess(x) for x in image], dim=0)
+            if not use_samhq:
+                image_embeddings = sam.module.image_encoder(input_images)
+            else:
+                image_embeddings, interm_embeddings = sam.module.image_encoder(
+                    input_images
+                )
+                interm_embeddings = interm_embeddings[0]
+        else:
+            input_images = torch.stack([sam.preprocess(x) for x in image], dim=0)
+            if not use_samhq:
+                image_embeddings = sam.image_encoder(input_images)
+            else:
+                image_embeddings, interm_embeddings = sam.image_encoder(input_images)
+                interm_embeddings = interm_embeddings[0]
+
+    sam_masks_list = None
+    for i in range(iters):
+        if i == 0:
+            pred_mask_list = coarse_masks
+        else:
+            pred_mask_list = sam_masks_list.to(torch.uint8)
+
+        input_dict, point_coords = sam_input_prepare(
+            image[0],
+            pred_mask_list,
+            image_embeddings,
+            resize_transform,
+            use_point=use_point,
+            use_box=use_box,
+            use_mask=use_mask,
+            add_neg=add_neg,
+            margin=margin,
+            gamma=gamma,
+            strength=strength,
+        )
+
+        sam_input = [input_dict]
+
+        if not is_train:
+            with torch.no_grad():
+                if ddp:
+                    if not use_samhq:
+                        sam_output = sam.module.forward_with_image_embeddings(
+                            image_embeddings, sam_input, multimask_output=True
+                        )[0]
+                    else:
+                        sam_output = sam.module.forward_with_image_embeddings(
+                            image_embeddings,
+                            interm_embeddings,
+                            sam_input,
+                            multimask_output=True,
+                        )[0]
+                else:
+                    if not use_samhq:
+                        sam_output = sam.forward_with_image_embeddings(
+                            image_embeddings, sam_input, multimask_output=True
+                        )[0]
+                    else:
+                        sam_output = sam.forward_with_image_embeddings(
+                            image_embeddings,
+                            interm_embeddings,
+                            sam_input,
+                            multimask_output=True,
+                        )[0]
+        else:
+            if ddp:
+                if not use_samhq:
+                    sam_output = sam.module.forward_with_image_embeddings(
+                        image_embeddings, sam_input, multimask_output=True
+                    )[0]
+                else:
+                    sam_output = sam.module.forward_with_image_embeddings(
+                        image_embeddings,
+                        interm_embeddings,
+                        sam_input,
+                        multimask_output=True,
+                    )[0]
+            else:
+                if not use_samhq:
+                    sam_output = sam.forward_with_image_embeddings(
+                        image_embeddings, sam_input, multimask_output=True
+                    )[0]
+                else:
+                    sam_output = sam.forward_with_image_embeddings(
+                        image_embeddings,
+                        interm_embeddings,
+                        sam_input,
+                        multimask_output=True,
+                    )[0]
+
+        # Select best mask based on IoU prediction
+        sam_masks = sam_output["masks"]
+        sam_ious = sam_output["iou_predictions"]
+        sam_masks3 = sam_masks
+
+        best_idx = sam_ious.argmax(dim=-1)
+        sam_masks = sam_masks[torch.arange(sam_masks.shape[0]), best_idx]
+        sam_ious = sam_ious[torch.arange(sam_ious.shape[0]), best_idx]
+
+        sam_masks_list = (sam_masks > 0.0).to(torch.uint8)
+
+    return sam_masks_list.cpu().numpy(), sam_ious.cpu().numpy(), sam_masks3
